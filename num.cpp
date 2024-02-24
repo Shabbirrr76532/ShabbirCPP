d0441ec8aaa74ae1100781f2eee5b2a0bea3ae10
Shabbir Zakir <127833025+Shabbirrr@users.noreply.github.com>
2024-02-24 14:26:09 +0530

    Add files via upload

---
+++ num.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[8];
+    cout<<"Enter your birthdate ";
+    for (int i;i<9;i++){
+cin>>a[i];
+    }
+int total=0;
+for(int i=0;i<9;i++){
+    total=a[i]+1;
+}
+cout<<"enter the year you want to check";
+int b[4];
+for (int j;j<9;j++){
+    cin>>a[j];
+    }
+    
+
+
+
+
+
+    return 0;
+}
